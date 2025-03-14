adcb8e4acc84bd272e77072de1f7e84c3b7acacd
JacksonFurrier <adam.szucs@proton.me>
2025-03-14 10:15:58 +0100

    Going forward with the reconstruction.

--- anim/recon_fbp.py
+++ anim/recon_fbp.py
@@ -1,37 +1,20 @@
 from manim import *
+import numpy as np
 
-class Bacteria(Dot):
-    def __init__(self, point=ORIGIN, ** kwargs):
-        Dot.__init__(self, point=point, color=GREEN, ** kwargs)
-        self.velocity = 6 * np.random.random_sample(3) - 3 # [-3, 3] interval
 
-class BouncingBacteria(Scene):
+np.random.seed(20)
+steps = np.random.choice(a=[-1, 0, 1], size=(100000,1000))
+stop = steps.cumsum(0)
+end_points= stop[-1]/stop[-1].max()
+end_pointsX = end_points[0:499]
+end_pointsY = end_points[500:-1]
+
+class recon_fbp(Scene):
     def construct(self):
-        # Create objects
-        box = Rectangle(width=5, height=5)
-        self.play(FadeIn(box))
-
-        bacteria_array = []
-        for i in range(0, 20):
-            bacteria_position = 4 * np.random.random_sample((3,)) - 2 # [-2, 2] interval
-            bacteria_array.append(Bacteria(bacteria_position))
-            self.play(FadeIn(bacteria_array[i]))
-
-        # Collision detection
-        def update_bacteria(bacteria, dt):
-            bacteria.acceleration = 2 * np.random.random_sample(3) - 1 # [-1, 1] interval
-            bacteria.velocity = bacteria.velocity + bacteria.acceleration * dt
-            bacteria.shift(bacteria.velocity * dt)
-
-            # Bounce off walls
-            if bacteria.get_left()[0] <= box.get_left()[0] or bacteria.get_right()[0] >= box.get_right()[0]:
-                bacteria.velocity[0] = -bacteria.velocity[0]
-
-            # Bounce off ground and roof
-            if bacteria.get_bottom()[1] <= box.get_bottom()[1] or bacteria.get_top()[1] >= box.get_top()[1]:
-                bacteria.velocity[1] = -bacteria.velocity[1]
-
-        # Attach collision detection to updater
-        for bacteria in bacteria_array:
-            bacteria.add_updater(update_bacteria)
-        self.wait(20)
+        radius= (end_pointsX*end_pointsX + end_pointsY * end_pointsY)**0.5
+        dots = VGroup()
+        for  x,y,r in zip(end_pointsX, end_pointsY,radius):
+            c= interpolate_color(YELLOW, RED, r)
+            dots.add(Dot(color=c,point=[3*x,3*y,0]).scale(0.7))
+        self.add(dots)
+        
